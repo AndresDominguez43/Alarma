2d0dc82e70aae1764dfe581e9777f71d4e550abe
AndresDominguez43 <andresdominguez0.98@gmail.com>
2024-08-06 17:05:18 -0300

    ElimineLittleFS

--- src/main.cpp
+++ src/main.cpp
@@ -2,36 +2,9 @@
 #include <WebServer.h>
 #include <WebSocket.h>
 #include <manejoAlarma.h>
-#include <LittleFS.h>
-
 
 void setup(){
   Serial.begin(115200);
-  delay(3000);
-
-  LittleFS.begin();
-
-  File file = LittleFS.open("index.html", "r");
-
-  if (!file) {
-    Serial.println("could not open file for reading");
-  } else {
-
-    while (file.available()) {
-      char buf[32];
-
-      size_t bytesRead = file.readBytes(buf, sizeof(buf) - 1);
-      buf[bytesRead] = '\0';
-
-      Serial.print(buf);
-
-      yield();
-    }
-
-    file.close();
-  }
-
-  LittleFS.end();
   configWiFi();
   configMDNS();
   initWebSocket();
